af417226e9b46d9e6ba7f1cab4a2ae0e6bafccaf
KenzyMesbah <kenzymesbah@gmail.com>
2025-11-27 12:00:14 +0200

    Add method comments in Payable interface

--- oop/src/Payable.java
+++ oop/src/Payable.java
@@ -1,2 +1,8 @@
 public interface Payable {
+    // يحسب اجمالي السعر بناءً على عدد الأشخاص وسعر الرحلة
+    double calculateTotalPrice();
+  
+    // ينفّذ عملية الدفع (مثلاً تغيير PaymentStatus)
+    boolean processPayment(double amount);
+
 }
